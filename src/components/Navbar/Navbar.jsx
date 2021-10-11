ba5863d2953feb1bbbe75a949e6384e55c725092
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-10-11 21:41:39 +0530

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <a>Quotify</a>
+      <a>Fetch a quote</a>
+      <a>Push a quote</a>
+      <a>About</a>
+    </div>
+  );
+}
