feedf5213e606ea41de9956ce1bdb1499ff11cd5
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-10-18 09:19:35 +0530

    logo added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import './Navbar.css';
+import "./Navbar.css";
+
+import logo from "./logo.svg";
 
 export default function Navbar() {
   return (
-    <div className="Navbar">
-      <a>Quotify</a>
-      <a>Fetch a quote</a>
-      <a>Push a quote</a>
-      <a>About</a>
-    </div>
+    <header className="Grid1">
+        <logo>
+        <img className="logo" src={logo} />
+        </logo>
+        <nav className="nav">
+        <ul className="ul">
+          <li>
+            <a className="li" href="#"> Quotify </a>
+          </li>
+          <li>
+            <a className="li" href="#">Fetch quote</a>
+          </li>
+          <li>
+            <a className="li" href="#">Push quote</a>
+          </li>
+          <li>
+            <a className="li" href="#">About</a>
+          </li>
+        </ul>
+        </nav>
+    </header>
   );
 }
